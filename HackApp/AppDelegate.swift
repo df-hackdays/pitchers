bcc4742a047f471c9f02b074b57c4dcfd9bbf34d
Nirmal Patel <nirmal.patel@scotiabank.com>
2018-09-29 03:04:42 -0400

    Added login page

--- HackApp/AppDelegate.swift
+++ HackApp/AppDelegate.swift
@@ -16,16 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        if let window = self.window {
-            window.backgroundColor = UIColor.white
-            
-            let nav = UINavigationController()
-            let mainView = ViewController()
-            nav.viewControllers = [mainView]
-            window.rootViewController = nav
-            window.makeKeyAndVisible()
-        }
         return true
     }
 
